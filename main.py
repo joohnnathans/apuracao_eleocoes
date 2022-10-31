7c4e4f2e2efacb11fe23ae3f03438dcf752afa35
Jonathan <joohnnathans@gmail.com>
2022-10-31 14:12:05 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import pandas as pd
+import time
+import gc
+
+while(True):
+    data = requests.get(
+        'https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json')
+        
+    json_data = json.loads(data.content)
+
+    candidato = []
+    partido = []
+    votos = []
+    porcentagem = []
+
+    for informacoes in json_data['cand']:
+        
+        if informacoes['seq'] in ['1', '2', '3', '4']:
+            candidato.append(informacoes['nm'])
+            votos.append(informacoes['vap'])
+            porcentagem.append(informacoes['pvap'])
+            
+    df_eleicao = pd.DataFrame(list(zip(candidato, votos, porcentagem)), columns = [
+        'Candidato', 'Nº de Votos', 'Porcentagem'
+    ])
+
+    print(df_eleicao)
+    print('-------------------------------------------------------------------------------------------------------')
+    time.sleep(120)
+    gc.collect()
